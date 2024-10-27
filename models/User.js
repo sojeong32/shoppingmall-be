d53b0c6eac1c051340d7da852598e575c6140606
박소정 <thwjd0235@naver.com>
2024-10-27 19:51:12 +0900

    feat: 회원가입, 로그인

--- models/User.js
+++ models/User.js
@@ -1,5 +1,9 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
+
 const userSchema = Schema(
   {
     email: { type: String, required: true, unique: true },
@@ -17,6 +21,13 @@ userSchema.method.toJSON = function () {
   delete obj.updatedAt;
   delete obj.createdAt;
   return obj;
+};
+
+userSchema.method.generateToken = async function () {
+  const token = await jwt.sign({ _id: this.id }, JWT_SECRET_KEY, {
+    expiresIn: "1d",
+  });
+  return token;
 };
 
 const User = mongoose.model("User", userSchema);
