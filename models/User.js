925f07fdcc46cf12201cbdb59f07d1165022ac39
박소정 <thwjd0235@naver.com>
2024-10-27 20:17:32 +0900

    fix: generateToken함수 수정

--- models/User.js
+++ models/User.js
@@ -15,15 +15,15 @@ const userSchema = Schema(
 );
 // 불필요한 정보를 빼고 갖고 오기
 userSchema.method.toJSON = function () {
-  const obj = this._doc;
-  delete obj.password;
+  const obj = this._doc; // _doc은 현재 mongoose문서에서 모든 데이터를 담고있는 객체를 가리킴
+  delete obj.password; // 백엔드에서 프론트엔드로 갈 때 패스워드는 항상 제외
   delete obj.__v;
   delete obj.updatedAt;
   delete obj.createdAt;
-  return obj;
+  return obj; // 위의 조건들을 모두 제거한 후 object를 리턴함
 };
 
-userSchema.method.generateToken = async function () {
+userSchema.methods.generateToken = async function () {
   const token = await jwt.sign({ _id: this.id }, JWT_SECRET_KEY, {
     expiresIn: "1d",
   });
