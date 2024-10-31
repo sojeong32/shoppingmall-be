368b02ee6260423713c3b6cd8eb5353131065934
박소정 <thwjd0235@naver.com>
2024-10-31 22:04:41 +0900

    feat: 페이지네이션 구현

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,5 +1,6 @@
 const Product = require("../models/Product");
 
+const PAGE_SIZE = 5;
 const productController = {};
 productController.createProduct = async (req, res) => {
   try {
@@ -38,10 +39,21 @@ productController.getProducts = async (req, res) => {
     const { page, name } = req.query;
     const cond = name ? { name: { $regex: name, $options: "i" } } : {};
     let query = Product.find(cond);
+    let response = { status: "success" };
+    if (page) {
+      query.skip((page - 1) * PAGE_SIZE).limit(PAGE_SIZE);
+      // 최종 몇 개 페이지인지
+      // 데이터가 총 몇 개 있는지
+      const totlaItemNum = await Product.find(cond).count();
+      // 데이터 총 갯수 / PAGE_SIZE
+      const totalPageNum = Math.ceil(totlaItemNum / PAGE_SIZE);
+      response.totalPageNum = totalPageNum;
+    }
 
     // 쿼리를 따로 실행시킴
     const productList = await query.exec();
-    res.status(200).json({ status: "success", data: productList });
+    response.data = productList;
+    res.status(200).json(response);
   } catch (error) {
     res.status(400).json({ status: "fail", error: error.message });
   }
