6de3bcf31cabdce5678db4735e69951d92bc33d5
박소정 <thwjd0235@naver.com>
2024-11-02 19:37:30 +0900

    fix: cartController 추가

--- routes/cart.api.js
+++ routes/cart.api.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/auth.controller");
+const cartController = require("../controllers/cart.controller");
 
 router.post("/", authController.authenticate, cartController.addItemToCart);
 
