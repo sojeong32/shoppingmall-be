c96b8dcd547fe64cfb4d1fda14e013a7c6f28513
박소정 <thwjd0235@naver.com>
2024-11-03 20:02:12 +0900

    feat: 카트페이지

--- routes/cart.api.js
+++ routes/cart.api.js
@@ -4,5 +4,6 @@ const authController = require("../controllers/auth.controller");
 const cartController = require("../controllers/cart.controller");
 
 router.post("/", authController.authenticate, cartController.addItemToCart);
+router.get("/", authController.authenticate, cartController.getCart);
 
 module.exports = router;
