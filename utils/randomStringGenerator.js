57db7e146a72c969b981f8768584fdb144673fde
박소정 <thwjd0235@naver.com>
2024-11-05 00:13:09 +0900

    fix: randomStringGenerator에서 오류 수정

--- utils/randomStringGenerator.js
+++ utils/randomStringGenerator.js
@@ -1,8 +1,6 @@
 const randomStringGenerator = () => {
   const randomString = Array.from(Array(10), () =>
-    Math.floor(Math.random() * 36)
-      .toString(36)
-      .join("")
+    Math.floor(Math.random() * 36).toString(36)
   );
 
   return randomString;
